c74567a54a08ad9ffaae7c7a95d3e4f4e9550d7e
Shubh007ox <110490602+Shubh007ox@users.noreply.github.com>
2022-12-24 04:32:17 -0800

    Add files via upload

---
+++ Finished Project with all the error resolved/components/StartingPage/StartingPageContent.js
@@ -0,0 +1,12 @@
+import classes from './StartingPageContent.module.css';
+
+const StartingPageContent = () => {
+  return (
+    <section className={classes.starting}>
+      <h1>Welcome on Board!</h1>
+      <h2>To the Login Demo!!</h2>
+    </section>
+  );
+};
+
+export default StartingPageContent;
